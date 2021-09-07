c447c5740f996b8c558c8109b5f2c3692c4e4875
Osman OKUYAN <f131220110@ktun.edu.tr>
2021-09-07 20:30:24 +0300

    init commit

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	//cards := newDeck()
+
+	//hand, remainingCards := deal(cards, 5)
+
+	//hand.print()
+	//remainingCards.print()
+
+	greeting := "Hi there osi!"
+
+	fmt.Println([]byte(greeting))
+
+}

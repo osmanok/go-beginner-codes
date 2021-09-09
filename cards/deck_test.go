73a9f9e2cbe6a80cf4e541f41c65b22c97d5ec66
Osman OKUYAN <f131220110@ktun.edu.tr>
2021-09-09 10:58:01 +0300

    day2

---
+++ cards/deck_test.go
@@ -0,0 +1,37 @@
+package main
+
+import "testing"
+
+func TestNewDeck(t *testing.T) {
+	d := newDeck()
+
+	if len(d) != 16 {
+		t.Errorf("Expected deck length of 16, but got %v", len(d))
+	}
+
+	if d[0] != "Ace of Spades" {
+		t.Errorf("Expected first card of Ace of Spades, but got %v", d[0])
+	}
+
+	if d[len(d)-1] != "Four of Clubs" {
+		t.Errorf("Expected last card of Four of Clubs, but got %v", d[len(d)-1])
+	}
+}
+
+func TestSaveToDeckAndNewDeckFromFile(t *testing.T) {
+	// create a new deck
+	d := newDeck()
+
+	// save the deck to file
+	d.saveToFile("_decktesting")
+
+	// create a new deck from file
+	loadedDeck := newDeckFromFile("_decktesting")
+
+	if len(loadedDeck) != 16 {
+		t.Errorf("Expected 16 cards in deck, got %v", len(loadedDeck))
+	}
+
+	// remove the file
+	// os.Remove("_decktesting")
+}
---
+++ cards/main.go
@@ -0,0 +1,7 @@
+package main
+
+func main() {
+	card := newDeck()
+	card.saveToFile("my_cards")
+
+}
